2c68decec1a420cd2351a03ddc9feddcf24eb5aa
Moymat <deckertmichel@gmail.com>
2022-02-15 13:07:17 +0100

    navbar mobile

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,11 @@
 import { setIconsHoverBehavior } from "./helpers";
 
 const dateFooter = document.querySelector("#date-footer");
-const navLinks = document.querySelectorAll("nav a");
 const languagesTitle = document.querySelector(".languages-title");
 const languagesList = document.querySelector(".languages-list");
 const languageItems = document.querySelectorAll(".language-item a");
 const projectContents = document.querySelectorAll(".project-content");
 const projectCards = document.querySelectorAll(".project-card");
-
-navLinks.forEach(link => {
-	link.setAttribute(
-		"href",
-		location.pathname === "/"
-			? `#${link.dataset.section}`
-			: `${window.location.origin}/#${link.dataset.section}`
-	);
-});
 
 dateFooter.innerHTML = new Date(Date.now()).getFullYear();
 
---
+++ src/js/navbar.js
@@ -0,0 +1,44 @@
+const navbarBurger = document.querySelector(".navbar-burger-icon");
+const burgerIcon = document.querySelector(".navbar-burger-icon");
+const burgerMenuItems = document.querySelectorAll(
+	".navbar-burger-menu .navbar-menu-item a"
+);
+const navLinks = document.querySelectorAll(".nav-link");
+
+navLinks.forEach(link => {
+	link.setAttribute(
+		"data-link",
+		location.pathname === "/"
+			? `#${link.dataset.section}`
+			: `${window.location.origin}/#${link.dataset.section}`
+	);
+});
+
+function toggleBurgerMenu() {
+	if (!navbarBurger.parentNode.classList.contains("open")) {
+		burgerIcon.classList.add("first-step");
+		setTimeout(() => {
+			burgerIcon.style.backgroundColor = "transparent";
+			burgerIcon.classList.add("second-step");
+		}, 250);
+	} else {
+		burgerIcon.classList.remove("second-step");
+		setTimeout(() => {
+			burgerIcon.style.removeProperty("background-color");
+			burgerIcon.classList.remove("first-step");
+		}, 250);
+	}
+	navbarBurger.parentNode.classList.toggle("open");
+}
+
+function navLinkClick(link) {
+	if (navbarBurger.parentNode.classList.contains("open")) {
+		toggleBurgerMenu();
+	}
+	window.location.href = link;
+}
+
+navbarBurger.addEventListener("click", toggleBurgerMenu);
+navLinks.forEach(link => {
+	link.addEventListener("click", navLinkClick.bind(null, link.dataset.link));
+});
